c3768bb1419a1d4efe0d3e2fc90331f945e83d80
Your Name <kmlshyadav@yahoo.com>
2020-04-02 21:11:41 +0530

    calling api for fetching facts list

---
+++ CTSAssignment/CTSAssignment/FactListService.swift
@@ -0,0 +1,37 @@
+//
+//  FactListService.swift
+//  CTSAssignment
+//
+//  Created by Kamlesh Kumar on 02/04/20.
+//  Copyright © 2020 Kamlesh Kumar. All rights reserved.
+//
+
+import Foundation
+
+protocol FactServiceProtocol : class {
+    func fetchConverter(_ completion: @escaping ((Result<Facts, ErrorResult>) -> Void))
+}
+
+final class FactListService : RequestHandler, FactServiceProtocol {
+    
+    static let shared = FactListService()
+    
+    let endpoint = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    var task : URLSessionTask?
+    
+    func fetchConverter(_ completion: @escaping ((Result<Facts, ErrorResult>) -> Void)) {
+        
+        // cancel previous request if already in progress
+        self.cancelFetchCurrencies()
+        task = RequestService().loadData(urlString: endpoint, completion:
+            self.networkResult(completion: completion)
+        )
+    }
+    
+    func cancelFetchCurrencies() {
+        if let task = task {
+            task.cancel()
+        }
+        task = nil
+    }
+}
