4a59cf717ed8e1fbfa755b06371f985a5e9884f2
Your Name <kmlshyadav@yahoo.com>
2020-04-02 21:13:23 +0530

    adding listmodel view and calling api

---
+++ CTSAssignment/CTSAssignment/ListViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  ListViewModel.swift
+//  CTSAssignment
+//
+//  Created by Kamlesh Kumar on 02/04/20.
+//  Copyright © 2020 Kamlesh Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct ListViewModel {
+    
+     var dataSource : GenericDataSource<Rows>?
+     var service: FactServiceProtocol?
+    
+    var onErrorHandling : ((ErrorResult?) -> Void)?
+    
+    init(service: FactServiceProtocol /*= CurrencyService.shared*/, dataSource : GenericDataSource<Rows>) {
+        self.dataSource = dataSource
+        self.service = service
+    }
+    
+    func fetchCurrencies() {
+        
+        guard let service = service else {
+            onErrorHandling?(ErrorResult.custom(string: "Missing service"))
+            return
+        }
+        
+        service.fetchConverter { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let factsModel) :
+                    print(factsModel)
+                    self.dataSource?.data.value = factsModel.rows
+                case .failure(let error) :
+                    self.onErrorHandling?(error)
+                }
+            }
+        }
+    }
+}
