17d289ac6b6b053c80515e6a6630c6073f38e517
Your Name <kmlshyadav@yahoo.com>
2020-04-02 19:58:46 +0530

    handeling request and parsing class method

---
+++ CTSAssignment/CTSAssignment/RequestHandler.swift
@@ -0,0 +1,54 @@
+//
+//  RequestHandler.swift
+//  CTSAssignment
+//
+//  Created by Kamlesh Kumar on 02/04/20.
+//  Copyright © 2020 Kamlesh Kumar. All rights reserved.
+//
+
+import Foundation
+
+
+class RequestHandler {
+
+    
+    func networkResult<T: Parceable>(completion: @escaping ((Result<[T], ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            
+            return { dataResult in
+                
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+                
+            }
+    }
+    
+    func networkResult<T: Parceable>(completion: @escaping ((Result<T, ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            
+            return { dataResult in
+                
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+                
+            }
+    }
+}
