0962db695cf437b27c69fd87c012c415e7265685
kamalesh <kamalesh.yadav@lycadigital.com>
2020-04-07 13:50:45 +0530

    code commenting Imageview extension

--- CTSAssignment/CTSAssignment/Resource/Extension_UiImageView.swift
+++ CTSAssignment/CTSAssignment/Resource/Extension_UiImageView.swift
@@ -9,34 +9,38 @@
 
 import UIKit
 
-
+/// declaring global variable for caching images which is downloaded
 let imageCache = NSCache<NSString, AnyObject>()
 
+/// UIImageview extension
+/// Description - Adding a API request for downloading images from server , where user acan directely pass image URL
+
 extension UIImageView {
+
+    /// To downlad image with image url
+    ///  - Parameter urlString: Pass Image URL as string which you image you want to down load
     func loadImageUsingCache(withUrl urlString : String) {
         self.image = nil
         self.image = UIImage(named: "noimg")
-        // check cached image
+        
+        /// checking for image already available in cache
         if let cachedImage = imageCache.object(forKey: urlString as NSString) as? UIImage {
             self.image = cachedImage
             return
         }
         guard let url = URL(string: urlString) else { return }
-        
-        // if not, download image from url
         URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
             if error != nil {
                 print(error!)
                 return
             }
-            
+//            populating Imageview with image data which we had from service or api to Cache on mainthread
             DispatchQueue.main.async {
                 if let image = UIImage(data: data!) {
                     imageCache.setObject(image, forKey: urlString as NSString)
                     self.image = image
                 }
             }
-            
-        }).resume()
+         }).resume()
     }
 }
